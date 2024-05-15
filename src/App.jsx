9072c463e29869dd974cf4ae496bc21cc7b7dc98
Muthamizh23 <muthamizh1023@gmail.com>
2024-05-15 14:02:28 +0530

    changed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskManager from './components/taskManager';
+import'./components/style.css';
+
+const App = () => {
+  return (
+    <div>
+      <TaskManager />
+    </div>
+  );
+};
+
+export default App;
